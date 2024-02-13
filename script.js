203f2c3b84c66c11f94683cc9b20c543c4e8d33a
Anirban Roy <156198466+AnirbanRoy169@users.noreply.github.com>
2024-02-14 00:28:06 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,59 @@
+var typed = new Typed('#lineOne', {
+    strings: ["Ajke kichu special banayni tmn"],
+    typeSpeed: 50,
+    showCursor: false,
+});
+var typed = new Typed('#lineTwo', {
+    strings: ["Jodio first e onk kichu kortechilam"],
+    startDelay: 5000,
+    typeSpeed: 50,
+    showCursor: false,
+});
+var typed = new Typed('#lineThree', {
+    strings: ["But kmn jno akta missing mone hocchilo"],
+    startDelay: 9000,
+    typeSpeed: 50,
+    showCursor: false,
+});
+var typed = new Typed('#lineFour', {
+    strings: ["R sai missing take khujte giye ata toiri hoiche"],
+    startDelay: 14000,
+    typeSpeed: 50,
+    showCursor: false,
+});
+var typed = new Typed('#lineFive', {
+    strings: ["Ai khane tmn special kichu nai"],
+    startDelay: 18000,
+    typeSpeed: 50,
+    showCursor: false,
+});
+var typed = new Typed('#lineSix', {
+    strings: ["But special ki bolto"],
+    startDelay: 22000,
+    typeSpeed: 50,
+    showCursor: false,
+});
+var typed = new Typed('#lineSeven', {
+    strings: ["Tui"],
+    startDelay: 24000,
+    typeSpeed: 50,
+    showCursor: false,
+});
+var typed = new Typed('#lineEight', {
+    strings: ["Amr kache sob theke special tui"],
+    startDelay: 25000,
+    typeSpeed: 50,
+    showCursor: false,
+});
+var typed = new Typed('#lineNine', {
+    strings: ["Sob somoy ai vabe e thakis"],
+    startDelay: 29000,
+    typeSpeed: 50,
+    showCursor: false,
+});
+var typed = new Typed('#lineTen', {
+    strings: ["LOVE YOU Watashi no ❤️"],
+    startDelay: 33000,
+    typeSpeed: 50,
+    showCursor: false,
+});
